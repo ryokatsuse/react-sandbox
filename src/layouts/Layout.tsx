c06606a70158891a5c84b4271a200530fe065868
Ryo Katsuse <ryokatsu0719@gmail.com>
2021-05-22 23:17:42 +0900

    fix:不要なファイル削除

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalHeader from "../components/GlobalHeader"
+
+export const Layout: React.FC = () => {
+  return (
+    <>
+      <GlobalHeader />
+      <main>
+        <p>test</p>
+      </main>
+    </>
+  )
+}
