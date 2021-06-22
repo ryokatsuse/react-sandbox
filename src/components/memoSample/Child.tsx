f6f185db47875dc07caecfcd9eeda3463972a12a
Ryo Katsuse <ryokatsu0719@gmail.com>
2021-06-22 23:29:32 +0900

    feat:React.memoの極端なコスト高いサンプルを追加

--- src/components/memoSample/Child.tsx
+++ src/components/memoSample/Child.tsx
@@ -5,6 +5,9 @@ type Props = {
 }
 
 const Child: VFC<Props> = props => {
+  // レンダリングコストが高いものをメモ化することで不要な再レンダリングをさせないようにする
+  let i = 0;
+  while (i < 1000000000) i++;
   console.log("render Child");
   return <p>Child: {props.count}</p>;
 };
